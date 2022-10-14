d8f075fc90605737cc44baa3c06d0161b67f0a34
Pranav Asrani <pranavnasrani@outlook.sg>
2022-10-14 21:00:57 +0800

    added new file:question added new widget. decorate

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
-import 'package:flutter/material.dart'; //required starter code
+import 'package:flutter/material.dart';
+import 'package:quiz_app/question.dart'; //required starter code
 
 //void main() {
 //  runApp(MyApp());
 //}
 
-void main() => runApp(MyApp()); //required starter code
+void main() => runApp(
+    MyApp()); //required starter code. we are rendering the main app onto the screen and running it
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -37,12 +39,12 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
             ElevatedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 2'),
